c79c4435d02d6011949e9a000566546071daee84
clive <clivekakeeto48@gmail.com>
2023-12-05 17:58:16 +0300

    initial commit

---
+++ instabuildhub/__init__.py
@@ -0,0 +1,8 @@
+# Adding convenience imports to the package
+from instabuildhub.core import (
+    ai,
+    domain,
+    chat_to_files,
+    steps,
+)
+from instabuildhub.data import file_repository, code_vector_repository
---
+++ instabuildhub/data/__init__.py
@@ -0,0 +1,11 @@
+"""
+instabuildhub.data
+-----------------
+
+Modules:
+    - code_vector_repository
+    - document_chunker
+    - file_repository
+    - supported_languages
+
+"""
